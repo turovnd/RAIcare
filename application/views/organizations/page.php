fc835435abcfdc3ae10f2f634730097fb692b02b
Nikolay <turov96@yandex.ru>
2017-06-11 14:39:21 +0300

    organization front

--- application/views/organizations/page.php
+++ application/views/organizations/page.php
@@ -16,7 +16,7 @@
     </div>
 
     <div class="bg-gray-dark p-15 m-b-30">
-        <div class="flex">
+        <div class="display-flex">
             <div class="col-xs-4 text-center b-r-light">
                 <h3 class="h3 m-0">2</h3>
                 <small class="m-0 ">Сотрудников</small>
@@ -86,6 +86,56 @@
     </div>
 
     <div class="col-xs-12 col-md-3">
+
+        <a href="/" class="btn btn--lg btn--brand col-xs-12 fl_n m-b-20">Статистика</a>
+
+        <div class="block">
+            <div class="block__heading">
+                <h4 class="m-0">
+                    <a href="" class="fl_r">
+                        <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    </a>
+                    Пансионаты
+                </h4>
+            </div>
+            <div id="pensions" class="block__body p-0">
+                <fieldset id="pension_1" class="p-0 m-0">
+                    <a href="/" class="p-15 display-block text-brand">Панисонат 1</a>
+                </fieldset>
+                <fieldset id="pension_2" class="p-0 m-0">
+                    <a href="/" class="p-15 display-block text-brand">Панисонат 2</a>
+                </fieldset>
+            </div>
+        </div>
+
+
+        <div class="block">
+            <div class="block__heading">
+                <h4 class="m-0">
+                    <a href="" class="fl_r">
+                        <i class="fa fa-user-plus" aria-hidden="true"></i>
+                    </a>
+                    Сотрудники
+                </h4>
+            </div>
+            <div id="assistants" class="block__body p-0">
+                <fieldset id="assistant_1" class="p-15 m-b-0">
+                    <a href="" class="fl_r m-l-5">
+                        <i class="fa fa-user-times" aria-hidden="true"></i>
+                    </a>
+                    <img src="/" alt="" class="img img--small img--circle fl_l m-r-10">
+                    <div class="display-table">Имя фамилия </div>
+                </fieldset>
+
+                <fieldset id="assistant_2" class="p-15 m-b-0">
+                    <a href="" class="fl_r m-l-5">
+                        <i class="fa fa-user-times" aria-hidden="true"></i>
+                    </a>
+                    <img src="/" alt="" class="img img--small img--circle fl_l m-r-10">
+                    <div class="display-table">Имя фамилия </div>
+                </fieldset>
+            </div>
+        </div>
 
     </div>
 
