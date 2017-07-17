589b2bcac813ea22fb51ea9bbd27c4644a78e205
Nikolay <turov96@yandex.ru>
2017-07-17 20:46:03 +0300

    fix patient-info block

--- application/views/reports/block/patient-info.php
+++ application/views/reports/block/patient-info.php
@@ -1,5 +1,6 @@
 <div class="block">
     <div class="block__body">
+        <div class="cursor-pointer pos-absolute right-0 btn btn--sm btn--default m-b-0 m-r-15" data-toggle="collapse" data-area="patientInfo" data-opened="false" style="z-index: 1">подробнее</div>
         <div class="form-group">
             <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Пациент</label>
             <div class="col-xs-12 col-sm-8 col-md-9">
@@ -12,48 +13,50 @@
                 <p class="form-group__control-static"><?= date('d M Y', strtotime($survey->patient->birthday)); ?></p>
             </div>
         </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Возраст</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static"><?= Methods_Time::relativeTimeWithPlural(intval((time()-strtotime($survey->patient->birthday))/Date::YEAR), false, 'yy'); ?></p>
+        <div class="fl_l collapse" id="patientInfo">
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Возраст</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static"><?= Methods_Time::relativeTimeWithPlural(intval((time()-strtotime($survey->patient->birthday))/Date::YEAR), false, 'yy'); ?></p>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Пол</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static"><?= $survey->patient->sex == 1 ? 'мужской' : 'женский'; ?></p>
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Пол</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static"><?= $survey->patient->sex == 1 ? 'мужской' : 'женский'; ?></p>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Причина прохождения оценки</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static"><?= Kohana::$config->load('form_type.new')[$survey->type]; ?></p>
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Причина прохождения оценки</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static"><?= Kohana::$config->load('form_type.new')[$survey->type]; ?></p>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Дата поступления</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static"><?= date('d M Y H:i', strtotime($survey->dt_first_survey)); ?></p>
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Дата поступления</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static"><?= date('d M Y H:i', strtotime($survey->dt_first_survey)); ?></p>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Дата начала прохождения оценки</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static"><?= date('d M Y H:i', strtotime($survey->dt_create)); ?></p>
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Дата начала прохождения оценки</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static"><?= date('d M Y H:i', strtotime($survey->dt_create)); ?></p>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Дата окончания прохождения оценки</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static"><?= date('d M Y H:i', strtotime($survey->dt_finish)); ?></p>
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Дата окончания прохождения оценки</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static"><?= date('d M Y H:i', strtotime($survey->dt_finish)); ?></p>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Пансионат</label>
-            <div class="col-xs-12 col-sm-8 col-md-9">
-                <p class="form-group__control-static">
-                    <a class="link" href="<?=URL::site('pension/'. $survey->pension->id); ?>"><?= $survey->pension->name; ?></a>
-                </p>
+            <div class="form-group">
+                <label class="form-group__label col-xs-12 col-sm-4 col-md-3">Пансионат</label>
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <p class="form-group__control-static">
+                        <a class="link" href="<?=URL::site('pension/'. $survey->pension->id); ?>"><?= $survey->pension->name; ?></a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
